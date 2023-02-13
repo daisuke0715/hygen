fda478d8c13769efabfbe200c8240ef3ec8da775
DaisukeKawamura <da1i9su9ke7.0715@icloud.com>
2023-02-14 01:29:10 +0900

    test creating test component by hygen

---
+++ src/components/Test/Test.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+/**
+ * << コンポーネントに関する説明コメントを記述してください。 >>
+*/
+
+const Test: React.FC = () => {
+  const test = '';
+  
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default Test;
